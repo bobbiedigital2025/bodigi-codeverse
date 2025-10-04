38facf80da30c2aa45c494385cb0c012880204bc
Bobbie Jo Gray creater of Bobbie Digital <bobbiefulton7@gmail.com>
2025-10-04 03:34:57 -0400

    Refactor main.jsx to use React and render App
    💡 This file tells React where to start and applies global CSS (we’ll create index.css next).

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BoDiGi CodeVerse</title>
-  </head>
-  <body class="bg-gray-50 text-gray-900">
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
